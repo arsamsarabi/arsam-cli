c3adbe248849842f4874eea80b50e2ca4cf23f52
Arsam Sarabi <arsamsarabi@me.com>
2019-03-23 12:41:10 +0000

    linting

--- app/data/cv.js
+++ app/data/cv.js
@@ -1,23 +1,23 @@
 const cv = {
   Education: [
-    "Access to higher education @ Oxford & Cherwell Valley College",
-    "BSc Computer Science @ Oxford Brookes University"
+    'Access to higher education @ Oxford & Cherwell Valley College',
+    'BSc Computer Science @ Oxford Brookes University',
   ],
   Experience: [
-    "Senior Frontend Engineer @ Sensyne Health",
-    "Development Manager / Scrum Master @ Contact Partners",
-    "UX/UI Developer @ Relayware (aka Zift Solutions)",
-    "Web Developer @ RFA Advertising and Marketing",
-    "Freelance Web Developer @ Sarabi Designs"
+    'Senior Frontend Engineer @ Sensyne Health',
+    'Development Manager / Scrum Master @ Contact Partners',
+    'UX/UI Developer @ Relayware (aka Zift Solutions)',
+    'Web Developer @ RFA Advertising and Marketing',
+    'Freelance Web Developer @ Sarabi Designs',
   ],
-  "Tech Stack": [
-    "Javascript - Node - Express - React - Angular",
-    "Mongo DB",
-    "GraphQL",
-    "CSS - Sass - Less - PostCss"
+  'Tech Stack': [
+    'Javascript - Node - Express - React - Angular',
+    'Mongo DB',
+    'GraphQL',
+    'CSS - Sass - Less - PostCss',
   ],
-  Projects: ["Coming soon!"],
-  Contact: ["Email: arsamsarabi@me.com", "Mobile: 077882*****"]
+  Projects: ['Coming soon!'],
+  Contact: ['Email: arsamsarabi@me.com', 'Mobile: 077882*****'],
 };
 
 export default cv;
--- app/index.js
+++ app/index.js
@@ -1,54 +1,51 @@
 #!/usr/bin/env node
-"use strict";
 
-import inquirer from "inquirer";
-import chalk from "chalk";
-import resume from "./data/cv.js";
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import resume from './data/cv';
 
 const response = chalk.bold.green;
 
 const prompts = {
-  type: "list",
-  name: "options",
-  message: "What do you want to know about me?",
-  choices: [...Object.keys(resume), "Exit"]
-};
-
-const main = () => {
-  console.log(
-    "Hello! My name is Arsam Sarabi, I am a Wed developer. Welcome to my CV!"
-  );
-  resumeHandler();
+  type: 'list',
+  name: 'options',
+  message: 'What do you want to know about me?',
+  choices: [...Object.keys(resume), 'Exit'],
 };
 
 const resumeHandler = () => {
-  inquirer.prompt(prompts).then(answer => {
-    if (answer.options === "Exit") {
+  inquirer.prompt(prompts).then((answer) => {
+    if (answer.options === 'Exit') {
       return;
     }
     const option = answer.options;
 
-    console.log(response("--------------------------------------"));
-    resume[`${option}`].forEach(info => {
-      console.log(response("|   => " + info));
+    console.log(response('--------------------------------------'));
+    resume[`${option}`].forEach((info) => {
+      console.log(response(`|   => ${info}`));
     });
-    console.log(response("--------------------------------------"));
+    console.log(response('--------------------------------------'));
 
     inquirer
       .prompt({
-        type: "list",
-        name: "exitBack",
-        message: "Go back or Exit?",
-        choices: ["Back", "Exit"]
+        type: 'list',
+        name: 'exitBack',
+        message: 'Go back or Exit?',
+        choices: ['Back', 'Exit'],
       })
-      .then(choice => {
-        if (choice.exitBack == "Back") {
+      .then((choice) => {
+        if (choice.exitBack === 'Back') {
           resumeHandler();
         } else {
           return;
         }
       });
   });
+};
+
+const main = () => {
+  console.log('Hello! My name is Arsam Sarabi, I am a Wed developer. Welcome to my CV!');
+  resumeHandler();
 };
 
 main();
