db0b9864d6a362dee880b829ea5ff374523f2baa
Arsam Sarabi <arsamsarabi@me.com>
2019-03-23 13:49:23 +0000

    emojis because why not

--- app/data/cv.js
+++ app/data/cv.js
@@ -2,7 +2,7 @@ const cv = {
   About: {
     data: [
       `
-Hi! my name is Arsam and I am a software engineer. I live in Oxford England and work fulltime as a Senior Frontend Engineer at Sensyne Health in the Oxford Science Park.
+Hi 👋 ! my name is Arsam and I am a software engineer 🤓 . I live in Oxford England and work fulltime as a Senior Frontend Engineer at Sensyne Health in the Oxford Science Park.
 
 I have been designing and developing websites and web applications since late 2012. I am currently focusing on creating applications using Javascript both on front and the backend. my Favourite frameworks are React for the frontend and Express for the backend. I have a keen interest on working with GraphQL in a commercial project and apply my expertise in a large scale application.
 
@@ -15,8 +15,8 @@ I have recently started to learn about "machine learning" and am eagerly expandi
   },
   Education: {
     data: [
-      'Access to higher education @ Oxford & Cherwell Valley College',
-      'BSc Computer Science @ Oxford Brookes University',
+      '🎓 Access to higher education @ Oxford & Cherwell Valley College',
+      '🎓 BSc Computer Science @ Oxford Brookes University',
     ],
     style: {
       colour: 'cyan',
@@ -24,11 +24,11 @@ I have recently started to learn about "machine learning" and am eagerly expandi
   },
   Experience: {
     data: [
-      'Senior Frontend Engineer @ Sensyne Health',
-      'Development Manager / Scrum Master @ Contact Partners',
-      'UX/UI Developer @ Relayware (aka Zift Solutions)',
-      'Web Developer @ RFA Advertising and Marketing',
-      'Freelance Web Developer @ Sarabi Designs',
+      '🖥 Senior Frontend Engineer @ Sensyne Health',
+      '💼 Development Manager / Scrum Master @ Contact Partners',
+      '💻 UX/UI Developer @ Relayware (aka Zift Solutions)',
+      '🕸 Web Developer @ RFA Advertising and Marketing',
+      '👾 Freelance Web Developer @ Sarabi Designs',
     ],
     style: {
       colour: 'green',
@@ -36,23 +36,23 @@ I have recently started to learn about "machine learning" and am eagerly expandi
   },
   'Tech Stack': {
     data: [
-      'Javascript - Node - Express - React - Angular',
-      'Mongo DB',
-      'GraphQL',
-      'CSS - Sass - Less - PostCss',
+      '🖥 Javascript - Node - Express - React - Angular',
+      '💻 Mongo DB',
+      '😍 GraphQL',
+      '💄 CSS - Sass - Less - PostCss',
     ],
     style: {
       colour: 'red',
     },
   },
   Projects: {
-    data: ['Coming soon!'],
+    data: ['😨 Coming soon!'],
     style: {
       colour: 'yellow',
     },
   },
   Contact: {
-    data: ['Email: arsamsarabi@me.com', 'Mobile: 077882*****'],
+    data: ['📮 Email: arsamsarabi@me.com', '📱 Mobile: 077882*****'],
     style: {
       colour: 'magenta',
     },
