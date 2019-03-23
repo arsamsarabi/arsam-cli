26cfe0d0368c12ee27b45de03a3d78972dc9f5b2
Arsam Sarabi <arsamsarabi@me.com>
2019-03-23 11:05:03 +0000

    v1

---
+++ app/index.js
@@ -0,0 +1,51 @@
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const resume = require("./data/cv.json");
+
+const response = chalk.bold.green;
+
+const prompts = {
+  type: "list",
+  name: "options",
+  message: "What do you want to know about me?",
+  choices: [...Object.keys(resume), "Exit"]
+};
+
+function main() {
+  console.log(
+    "Hello! My name is Arsam Sarabi, I am a Wed developer. Welcome to my CV!"
+  );
+  resumeHandler();
+}
+
+function resumeHandler() {
+  inquirer.prompt(prompts).then(answer => {
+    if (answer.options === "Exit") {
+      return;
+    }
+    const option = answer.options;
+
+    console.log(response("--------------------------------------"));
+    resume[`${option}`].forEach(info => {
+      console.log(response("|   => " + info));
+    });
+    console.log(response("--------------------------------------"));
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.exitBack == "Back") {
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+}
+
+main();
