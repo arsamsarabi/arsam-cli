9b6bdfce88af41133e73a04dc6c207ab06e6e029
Arsam Sarabi <arsamsarabi@me.com>
2019-08-17 12:32:16 +0100

    updating CV

--- app/data/cv.js
+++ app/data/cv.js
@@ -2,7 +2,7 @@ const cv = {
   About: {
     data: [
       `
-Hi 👋 ! my name is Arsam and I am a software engineer 🤓 . I live in Oxford England and work fulltime as a Senior Frontend Engineer at Sensyne Health in the Oxford Science Park.
+Hi 👋 ! my name is Arsam and I am a software engineer. I live in Oxford England and work fulltime as Lead Frontend Architect at Sensyne Health in the Oxford Science Park.
 
 I have been designing and developing websites and web applications since late 2012. I am currently focusing on creating applications using Javascript both on front and the backend. my Favourite frameworks are React for the frontend and Express for the backend. I have a keen interest on working with GraphQL in a commercial project and apply my expertise in a large scale application.
 
